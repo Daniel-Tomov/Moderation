634e1affb915e24d8aab6b5fcd201474bcf7c275
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-02-16 20:24:09 -0500

    Update Main.java

--- src/me/cageydinosaur/moderation/Main.java
+++ src/me/cageydinosaur/moderation/Main.java
@@ -8,6 +8,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+	public chatList = 
+	
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(new Events(this), this);
 		getCommand("moderation").setExecutor(new Cmd(this));
